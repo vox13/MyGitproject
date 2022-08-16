4c1b45de634a4b7927431d56a0cd51e2db4bc106
vox13 <val.ohotnikov@gmail.com>
2022-08-16 22:05:13 +0300

    Start Project

---
+++ app/src/main/java/com/bfg9000/android/mygitproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bfg9000.android.mygitproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
